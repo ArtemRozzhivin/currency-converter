e66136d5ed1eba5ebfa63139555d2de1c5d140f2
ArtemRozzhivin <artiomrozzhyvin@gmail.com>
2023-07-25 19:54:43 +0300

    connected API | added converter logic

--- src/components/Currency.jsx
+++ src/components/Currency.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const Currency = ({ isConverted }) => {
+const Currency = ({ currency, setCurrency, value, setValue, exchangeRate, isConverted }) => {
+  const currencies = Object.keys(exchangeRate.course.data);
+
   return (
     <div className='flex flex-col gap-4'>
       <div className='text-[#989898] text[15px]'>
@@ -9,15 +11,18 @@ const Currency = ({ isConverted }) => {
       <div className='flex gap-4'>
         <select
           id='currency'
-          class='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'>
-          <option selected>Choose a country</option>
-          <option value='US'>United States</option>
-          <option value='CA'>Canada</option>
-          <option value='FR'>France</option>
-          <option value='DE'>Germany</option>
+          onChange={(e) => setCurrency(e.target.value)}
+          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'>
+          {currencies.map((item) => (
+            <option selected={currency === item} key={item}>
+              {item}
+            </option>
+          ))}
         </select>
 
         <input
+          value={value}
+          setValue={setValue}
           type='number'
           className='bg-[#EFEFEF] rounded-lg px-4 py-2 font-semibold text-xl text-right block w-full'
         />
